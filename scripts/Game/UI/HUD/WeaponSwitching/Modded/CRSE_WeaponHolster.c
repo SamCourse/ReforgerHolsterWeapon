53ae553ec15a3761c8d542dabc823c61c970d699
Sam Course <notcourseps@gmail.com>
2022-05-25 14:26:26 +0200

    Added weapon holster functionality

---
+++ scripts/Game/UI/HUD/WeaponSwitching/Modded/CRSE_WeaponHolster.c
@@ -0,0 +1,59 @@
+class CRSE_WeaponHolster: SCR_InfoDisplay {
+	private InputManager inputManager;
+	private SCR_CharacterControllerComponent characterController;
+	private BaseWeaponManagerComponent weaponManager;
+	private IEntity player;
+	private BaseWeaponComponent lastWeapon;
+	
+	void CRSE_WeaponHolster(IEntity owner) {
+		this.player = owner;
+	}
+	
+	void Init() {
+		characterController = SCR_CharacterControllerComponent.Cast(player.FindComponent(SCR_CharacterControllerComponent));
+		this.registerEventListener();
+	}
+
+    void holsterKeyPressed() {
+		if (!characterController || characterController.IsChangingItem())
+			return;
+
+		weaponManager = characterController.GetWeaponManagerComponent();
+		if (!weaponManager)
+			return;
+		
+		BaseWeaponComponent currentWeapon = weaponManager.GetCurrentWeapon();
+		if (!currentWeapon) {
+			this.unholsterWeapon();
+		} else {
+			this.holsterWeapon();
+		}
+    }
+	
+	private void holsterWeapon() {
+		this.lastWeapon = weaponManager.GetCurrentSlot();
+		characterController.SelectWeapon(null);
+	}
+	
+	private void unholsterWeapon() {
+		if (!this.lastWeapon)
+			return;
+		
+		this.lastWeapon = null;
+		characterController.SelectWeapon(this.lastWeapon);
+	}
+	
+	
+    void registerEventListener() {
+        inputManager = GetGame().GetInputManager();
+		
+        if(!inputManager)
+            return;
+		
+        inputManager.AddActionListener("CharacterHolsterWeapon", EActionTrigger.UP, holsterKeyPressed);
+    }
+	
+    void unregisterEventListener() {
+        inputManager.RemoveActionListener("CharacterHolsterWeapon", EActionTrigger.UP, holsterKeyPressed);
+    }
+}
---
+++ scripts/Game/UI/HUD/WeaponSwitching/Modded/SRC_WeaponSwitchingBaseUI.c
@@ -0,0 +1,14 @@
+modded class SCR_WeaponSwitchingBaseUI: SCR_InfoDisplay {
+	private ref CRSE_WeaponHolster weaponHolster;
+	
+    override protected void Init(IEntity owner) {
+		super.Init(owner);
+		weaponHolster = new CRSE_WeaponHolster(owner);
+		weaponHolster.Init();
+    }
+	
+	override protected void RemoveActionListeners() {
+		super.RemoveActionListeners();
+		weaponHolster.unregisterEventListener();
+	}
+}
