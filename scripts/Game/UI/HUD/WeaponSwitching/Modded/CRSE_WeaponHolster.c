77d2121aed2e2ac2345f00d9f694d139cccff5c2
Sam Course <notcourseps@gmail.com>
2022-05-27 20:44:47 +0200

    Fix strange formatting

--- scripts/Game/UI/HUD/WeaponSwitching/Modded/CRSE_WeaponHolster.c
+++ scripts/Game/UI/HUD/WeaponSwitching/Modded/CRSE_WeaponHolster.c
@@ -4,12 +4,12 @@ class CRSE_WeaponHolster: SCR_InfoDisplay {
 	private BaseWeaponManagerComponent weaponManager;
 	private IEntity player;
 	private BaseWeaponComponent lastWeapon;
-	
+
 	void CRSE_WeaponHolster(IEntity owner) {
 		this.player = owner;
 	}
-	
-	void Init() {
+
+    void Init() {
 		characterController = SCR_CharacterControllerComponent.Cast(player.FindComponent(SCR_CharacterControllerComponent));
 		this.registerEventListener();
 	}
@@ -21,38 +21,38 @@ class CRSE_WeaponHolster: SCR_InfoDisplay {
 		weaponManager = characterController.GetWeaponManagerComponent();
 		if (!weaponManager)
 			return;
-		
+
 		BaseWeaponComponent currentWeapon = weaponManager.GetCurrentWeapon();
 		if (!currentWeapon) {
 			this.unholsterWeapon();
 		} else {
 			this.holsterWeapon();
 		}
-    }
-	
-	private void holsterWeapon() {
+	}
+
+    private void holsterWeapon() {
 		this.lastWeapon = weaponManager.GetCurrentSlot();
 		characterController.SelectWeapon(null);
 	}
-	
-	private void unholsterWeapon() {
+
+    private void unholsterWeapon() {
 		if (!this.lastWeapon)
 			return;
-		
+
 		characterController.SelectWeapon(this.lastWeapon);
 		this.lastWeapon = null;
 	}
-	
-	
+
+
     void registerEventListener() {
         inputManager = GetGame().GetInputManager();
-		
+
         if(!inputManager)
             return;
-		
+
         inputManager.AddActionListener("CharacterHolsterWeapon", EActionTrigger.UP, holsterKeyPressed);
     }
-	
+
     void unregisterEventListener() {
         inputManager.RemoveActionListener("CharacterHolsterWeapon", EActionTrigger.UP, holsterKeyPressed);
     }
--- scripts/Game/UI/HUD/WeaponSwitching/Modded/SRC_WeaponSwitchingBaseUI.c
+++ scripts/Game/UI/HUD/WeaponSwitching/Modded/SRC_WeaponSwitchingBaseUI.c
@@ -1,12 +1,12 @@
 modded class SCR_WeaponSwitchingBaseUI: SCR_InfoDisplay {
 	private ref CRSE_WeaponHolster weaponHolster;
-	
+
     override protected void Init(IEntity owner) {
 		super.Init(owner);
 		weaponHolster = new CRSE_WeaponHolster(owner);
 		weaponHolster.Init();
     }
-	
+
 	override protected void RemoveActionListeners() {
 		super.RemoveActionListeners();
 		weaponHolster.unregisterEventListener();
